b0c406b3b7b1bcec786d4af96e5268b9b1448c9d
kayleen <8iscogb9@anonaddy.me>
2022-03-11 11:33:59 -0600

    finished employees.js

--- orders.js
+++ orders.js
@@ -6,46 +6,50 @@
     to make tickets from order information.
 */
 
-
+let sep = `----------------`
+console.log(sep)
 //////////////////PROBLEM 1////////////////////
+console.log(`Problem 1:`)
+
 /*  
-    Create a new class called `Ticket`.
-    Make sure to call your constructor, and 
-    require these 3 parameters: items, orderTime, 
-    customerId. Additionally, set up a property
-    called `status` that always has an initial
-    value of 'queued'.
+    Create a new class called `Ticket`. Make sure to call your constructor, and require these 3 parameters: items, orderTime, 
+    customerId. Additionally, set up a property called `status` that always has an initial value of 'queued'.
 
-    Create a method on the class called `updateStatus`.
-    The method should have one parameter, `newStatus`,
-    which will be a string.
+    Create a method on the class called `updateStatus`. The method should have one parameter, `newStatus`, which will be a string.
 
-    Inside the method, set the value of `this.status`
-    to be the new status that was sent in. Then
-    console.log something like: 
-    'The order for customer [CUSTOMERID] is
-    now [STATUS].'
-    Where CUSTOMERID and STATUS reference the values
-    stored on the object.
+    Inside the method, set the value of `this.status` to be the new status that was sent in. Then console.log something like: 
+    'The order for customer [CUSTOMERID] is now [STATUS].'
+    Where CUSTOMERID and STATUS reference the values stored on the object.
 */
 
 //CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerId, status){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = `q'd`
+    }
 
-
+    updateStatus(newStatus) {
+        let x = this.status
+        this.status = newStatus
+        console.log(`\nThe order for customer ${this.customerId} has changed from ${x} to ${this.status}\n`)
+    }
+}
 
 /*
-    Create a new instance of your class.
-    Save it to a variable called `firstTicket`.
+    Create a new instance of your class. Save it to a variable called `firstTicket`.
 
-    You can use this sample data or make
-    up your own:
+    You can use this sample data or make up your own:
     food ordered: pizza, bread, and soda
     ordered at: 7:03 PM
     customer: 575
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket(1, `12:25`, 001)
+console.log(firstTicket)
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +58,6 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus(`done`)
+
+console.log(firstTicket)
